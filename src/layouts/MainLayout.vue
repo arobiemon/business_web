45f614858c7d6729957d8b6b8929f0ba29f524e4
Rahulreza <rahulreza001@gmail.com>
2024-08-31 18:19:02 +0600

    footer added new

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -18,11 +18,15 @@
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
-    <q-footer elevated>
-        <q-toolbar>
-          <q-toolbar-title>Footer</q-toolbar-title>
-        </q-toolbar>
+
+    <q-footer bordered class="bg-grey-3 text-primary">
+        <q-tabs no-caps active-color="primary" indicator-color="transparent" class="text-grey-8" v-model="tab">
+          <q-tab name="images" label="Images" />
+          <q-tab name="videos" label="Videos" />
+          <q-tab name="articles" label="Articles" />
+        </q-tabs>
       </q-footer>
+
     <q-drawer
       v-model="leftDrawerOpen"
       show-if-above
