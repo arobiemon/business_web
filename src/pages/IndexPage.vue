a5f0ab80d0ce44450d5c2c85ea79b0d6a9fc359a
Md. Arobi Emon <94915353+arobiemon@users.noreply.github.com>
2024-09-02 13:08:19 +0600

    next update

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -25,7 +25,7 @@
           />
         </div>
       </div>
-      <div class="q-mt-md icons-container">
+      <div class="q-mt-md row flex-center icons-container">
         <q-icon name="facebook" size="30px" color="black" />
         <q-icon name="facebook" size="30px" color="black" />
         <q-icon name="facebook" size="30px" color="black" />
@@ -33,19 +33,18 @@
       </div>
     </div>
     <div>
-      <div class="centered-text text-h4">
+      <div class="centered-text text-h4 row flex-center">
         Turn data into better business outcomes.
       </div>
-      <div class="centered-lorem">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati
-        dignissimos rem qui modi ipsam laboriosam vel dolorem necessitatibus
-        illo tempora, sed sit neque velit debitis molestias, numquam, dolores
-        fugiat animi! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Inventore similique corporis neque fugit sapiente aspernatur quam alias,
-        pariatur recusandae nemo nostrum veritatis quo unde provident. Aut iure
-        doloribus culpa quisquam?
+      <div class="row flex-center centered-lorem">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur,
+        animi. Hic consequuntur et soluta, ea nisi ipsam saepe, nihil obcaecati,
+        sunt corporis quas accusantium harum eos sit fuga vero vitae. Lorem
+        ipsum dolor sit amet, consectetur adipisicing elit. Magni deserunt eaque
+        tenetur et eum dolorum! Assumenda numquam neque ratione soluta incidunt
+        ducimus omnis unde porro.
       </div>
-      <div class="q-pa-xl row centered-cols">
+      <div class="q-pa-xl centered-cols row flex-center">
         <div class="col">
           <div class="text-bold text-h3">300%</div>
           <div>increase in new pipeline generated</div>
@@ -60,6 +59,19 @@
           <div class="text-bold text-h3">60%</div>
           <div>Decrease in cost per lead</div>
         </div>
+      </div>
+      <div class="row flex-center">
+        <q-btn
+          label="Explore the platfrom"
+          @click="onClick"
+          no-caps
+          no-wrap
+          unelevated
+          :style="{
+            background: 'linear-gradient(90deg, #1e3a8a, #3b82f6)',
+            color: '#fff',
+          }"
+        />
       </div>
     </div>
   </div>
@@ -87,30 +99,28 @@ defineOptions({
 
 .icons-container {
   display: flex;
-  justify-content: center;
-  align-items: center;
   gap: 60px;
 }
 
 .centered-text {
-  text-align: center;
-  /* font-size: 20px; */
   font-weight: bold;
   margin-top: 20px;
 }
 
 .centered-lorem {
-  text-align: center;
   margin-top: 10px;
-  padding: 0 20px;
+  padding: 10px 150px 10px 150px;
 }
 .centered-cols {
-  display: flex;
-  justify-content: center;
   text-align: center;
   color: rgba(44, 148, 204, 0.856);
 }
 .col {
   flex: 0 1 300px;
+}
+.btn-wrapper {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
 }
 </style>
