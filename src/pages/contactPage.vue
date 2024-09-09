b8fd3344d10321637840502b675b1b6bbb6f2106
Md. Arobi Emon <94915353+arobiemon@users.noreply.github.com>
2024-09-09 11:23:56 +0600

    contact page update

--- src/pages/contactPage.vue
+++ src/pages/contactPage.vue
@@ -2,19 +2,17 @@
   <q-page padding>
     <div class="q-pa-md flex flex-center" style="height: 100vh">
       <div class="q-pa-md" style="max-width: 400px; width: 100%">
-        <q-form
-          @submit="onSubmit"
-          @reset="onReset"
-          class="q-gutter-md"
-        >
+        <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
           <q-input
             outlined
             type="text"
             v-model="name"
             label="Name *"
+            rounded
+            bg-color="green-1"
             lazy-rules
             :rules="[
-              val => val !== null && val !== '' || 'Please type your name',
+              (val) => (val !== null && val !== '') || 'Please type your name',
             ]"
           />
 
@@ -23,18 +21,26 @@
             type="number"
             v-model="age"
             label="Age *"
+            rounded
+            bg-color="green-1"
             lazy-rules
             :rules="[
-              val => val !== null && val !== '' || 'Please type your age',
-              val => val > 0 && val < 100 || 'Please type a real age'
+              (val) => (val !== null && val !== '') || 'Please type your age',
+              (val) => (val > 0 && val < 100) || 'Please type a real age',
             ]"
           />
 
           <q-toggle v-model="accept" label="I accept the license and terms" />
 
           <div>
-            <q-btn label="Submit" type="submit" color="primary"/>
-            <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+            <q-btn label="Submit" type="submit" rounded no-cap color="green" />
+            <q-btn
+              label="Reset"
+              type="reset"
+              color="primary"
+              flat
+              class="q-ml-sm"
+            />
           </div>
         </q-form>
       </div>
@@ -43,47 +49,46 @@
 </template>
 
 <script>
-import { useQuasar } from 'quasar'
-import { ref } from 'vue'
+import { useQuasar } from "quasar";
+import { ref } from "vue";
 
 export default {
-  setup () {
-    const $q = useQuasar()
+  setup() {
+    const $q = useQuasar();
 
-    const name = ref(null)
-    const age = ref(null)
-    const accept = ref(false)
+    const name = ref(null);
+    const age = ref(null);
+    const accept = ref(false);
 
     return {
       name,
       age,
       accept,
 
-      onSubmit () {
+      onSubmit() {
         if (accept.value !== true) {
           $q.notify({
-            color: 'red-5',
-            textColor: 'white',
-            icon: 'warning',
-            message: 'You need to accept the license and terms first'
-          })
-        }
-        else {
+            color: "red-5",
+            textColor: "white",
+            icon: "warning",
+            message: "You need to accept the license and terms first",
+          });
+        } else {
           $q.notify({
-            color: 'green-4',
-            textColor: 'white',
-            icon: 'cloud_done',
-            message: 'Submitted'
-          })
+            color: "green-4",
+            textColor: "white",
+            icon: "cloud_done",
+            message: "Submitted",
+          });
         }
       },
 
-      onReset () {
-        name.value = null
-        age.value = null
-        accept.value = false
-      }
-    }
-  }
-}
+      onReset() {
+        name.value = null;
+        age.value = null;
+        accept.value = false;
+      },
+    };
+  },
+};
 </script>
